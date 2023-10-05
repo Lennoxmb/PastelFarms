c058662e0a9ddfddf072a67286cc4f267a5e2eed
Lennox Ballard <noellemb@gmail.com>
2023-10-05 17:19:08 -0500

    starting commit

---
+++ src/main/java/com/example/pastelfarms/PastelFarmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pastelfarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastelFarmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastelFarmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pastelfarms/PastelFarmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pastelfarms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastelFarmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
